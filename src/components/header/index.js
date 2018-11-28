36c817a24791bc586410cd13ad657896ff1ddf72
Christopher Brown <chbphone55@gmail.com>
2018-11-28 14:21:06 -0700

    open close drawer fixed

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,12 +5,10 @@ import { route } from 'preact-router';
 import TopAppBar from 'preact-material-components/TopAppBar';
 import Drawer from 'preact-material-components/Drawer';
 import List from 'preact-material-components/List';
-import Dialog from 'preact-material-components/Dialog';
 import Switch from 'preact-material-components/Switch';
 
 // Material CSS
 import 'preact-material-components/Switch/style.css';
-import 'preact-material-components/Dialog/style.css';
 import 'preact-material-components/Drawer/style.css';
 import 'preact-material-components/List/style.css';
 import 'preact-material-components/TopAppBar/style.css';
@@ -19,19 +17,12 @@ import 'preact-material-components/TopAppBar/style.css';
 import style from './style';
 
 export default class Header extends Component {
-  closeDrawer() {
-    this.drawer.MDComponent.open = false;
-    this.state = {
-      darkThemeEnabled: false
-    };
-  }
+  drawerRef = drawer => {
+    this.drawer = drawer;
+  };
 
-  openDrawer = () => (this.drawer.MDComponent.open = true);
-
-  openSettings = () => this.dialog.MDComponent.show();
-
-  drawerRef = drawer => (this.drawer = drawer);
-  dialogRef = dialog => (this.dialog = dialog);
+  closeDrawer = () => this.drawer.MDComponent.open = false;
+  openDrawer = () => this.drawer.MDComponent.open = true;
 
   linkTo = path => () => {
     route(path);
@@ -40,16 +31,6 @@ export default class Header extends Component {
 
   goHome = this.linkTo('/');
   goToMyProfile = this.linkTo('/movies/');
-
-  toggleDarkTheme = () => {
-    this.setState({
-      darkThemeEnabled: !this.state.darkThemeEnabled
-    }, () =>
-      this.state.darkThemeEnabled
-        ? document.body.classList.add('mdc-theme--dark')
-        : document.body.classList.remove('mdc-theme--dark')
-    );
-  }
 
   render(props) {
     return (
@@ -63,9 +44,6 @@ export default class Header extends Component {
               <TopAppBar.Title>Movie Collector</TopAppBar.Title>
             </TopAppBar.Section>
             <TopAppBar.Section align-end shrink-to-fit>
-              <span className={`${style.pointer} ${style.settingsButton}`} onClick={this.openSettings}>
-                Settings &nbsp; <TopAppBar.Icon>settings</TopAppBar.Icon>
-              </span>
             </TopAppBar.Section>
           </TopAppBar.Row>
         </TopAppBar>
@@ -81,17 +59,6 @@ export default class Header extends Component {
             </Drawer.DrawerItem>
           </Drawer.DrawerContent>
         </Drawer>
-        <Dialog ref={this.dialogRef}>
-          <Dialog.Header>Settings</Dialog.Header>
-          <Dialog.Body>
-            <div>
-              Enable dark theme <Switch onClick={this.toggleDarkTheme} />
-            </div>
-          </Dialog.Body>
-          <Dialog.Footer>
-            <Dialog.FooterButton accept>OK</Dialog.FooterButton>
-          </Dialog.Footer>
-        </Dialog>
       </div>
     );
   }
