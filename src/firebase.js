5a7b6a69a2fb45d55727f59b3df2518809abc040
Christopher Brown <chbphone55@gmail.com>
2018-11-28 08:17:17 -0700

    firebase auth setup

--- src/firebase.js
+++ src/firebase.js
@@ -12,3 +12,6 @@ const config = {
 firebase.initializeApp(config);
 
 export default firebase;
+
+export const provider = new firebase.auth.GoogleAuthProvider();
+export const auth = firebase.auth();
