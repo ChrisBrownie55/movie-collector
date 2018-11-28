dbced19930620cc2d8ec03caa07268cd7cbb0527
Christopher Brown <chbphone55@gmail.com>
2018-11-28 06:58:44 -0700

    tabs to spaces and remove some boilerplate

--- src/components/app.js
+++ src/components/app.js
@@ -9,27 +9,27 @@ import NotFound from '../routes/404';
 // import Profile from 'async!../routes/profile';
 
 export default class App extends Component {
-	/** Gets fired when the route changes.
-	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-	 *	@param {string} event.url	The newly routed URL
-	 */
-	handleRoute = e => {
-		this.setState({
-			currentUrl: e.url
-		});
-	};
+  /** Gets fired when the route changes.
+   *  @param {Object} event    "change" event from [preact-router](http://git.io/preact-router)
+   *  @param {string} event.url  The newly routed URL
+   */
+  handleRoute = e => {
+    this.setState({
+      currentUrl: e.url
+    });
+  };
 
-	render() {
-		return (
-			<div id="app">
-				<Header selectedRoute={this.state.currentUrl}/>
-				<Router onChange={this.handleRoute}>
-					<Home path="/" />
-					<Profile path="/profile/" user="me" />
-					<Profile path="/profile/:user" />
+  render() {
+    return (
+      <div id="app">
+        <Header selectedRoute={this.state.currentUrl}/>
+        <Router onChange={this.handleRoute}>
+          <Home path="/" />
+          <Profile path="/profile/" user="me" />
+          <Profile path="/profile/:user" />
           <NotFound default />
-				</Router>
-			</div>
-		);
-	}
+        </Router>
+      </div>
+    );
+  }
 }
