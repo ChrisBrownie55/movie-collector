206aaedc12359eb27411f95655284fd57dae40f1
Christopher Brown <chbphone55@gmail.com>
2018-11-28 21:31:44 -0700

    styles for login

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -4,6 +4,8 @@ import Button from 'preact-material-components/Button';
 import 'preact-material-components/Button/style.css';
 import Typography from 'preact-material-components/Typography';
 import 'preact-material-components/Typography/style.css';
+import Elevation from 'preact-material-components/Elevation';
+import 'preact-material-components/Elevation/style.css';
 
 import style from './style.css';
 
@@ -14,7 +16,7 @@ export default class Login extends Component {
         <div className={style.Login}>
           <Typography headline5>Welcome to Movie Collector</Typography>
           <Typography body1>If you haven't registered yet, don't worry, logging in with Google will create you an account.</Typography>
-          <Button raised className={style.googleSignIn}>
+          <Button className={style.googleSignIn}>
             <img src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg" alt="Google logo" />
             <span>Sign in with Google</span>
           </Button>
