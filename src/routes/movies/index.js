111cad3b4f46aef9539ad85597afb25cf1593300
Christopher Brown <chbphone55@gmail.com>
2018-11-30 11:57:17 -0700

    styles

--- src/routes/movies/index.js
+++ src/routes/movies/index.js
@@ -6,7 +6,10 @@ export default class Movies extends Component {
   render() {
     return (
       <div class={`${style.movies} page`}>
-        <Movie posterSrc="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPcvWv3fwAHmQMx4nFrOwAAAABJRU5ErkJggg==" posterAlt="placeholder" movieName="Movie Title" />
+        <section class={style.moviesList}>
+          <Movie posterSrc="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPcvWv3fwAHmQMx4nFrOwAAAABJRU5ErkJggg==" posterAlt="placeholder" movieName="Movie Title" />
+          <Movie posterSrc="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPcvWv3fwAHmQMx4nFrOwAAAABJRU5ErkJggg==" posterAlt="placeholder" movieName="Movie Title" />
+        </section>
       </div>
     );
   }
