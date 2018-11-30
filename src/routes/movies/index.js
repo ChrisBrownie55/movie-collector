b46026800c2829c48d371386ca6396c816630e2e
Christopher Brown <chbphone55@gmail.com>
2018-11-30 11:46:40 -0700

    styling for movie

--- src/routes/movies/index.js
+++ src/routes/movies/index.js
@@ -6,7 +6,7 @@ export default class Movies extends Component {
   render() {
     return (
       <div class={`${style.movies} page`}>
-        <Movie posterSrc="https://picsum.photos/400/300" posterAlt="placeholder" movieName="Movie Title" />
+        <Movie posterSrc="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPcvWv3fwAHmQMx4nFrOwAAAABJRU5ErkJggg==" posterAlt="placeholder" movieName="Movie Title" />
       </div>
     );
   }
