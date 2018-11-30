cf485e50031ea9f1c986098154a822d2fc6c8378
Christopher Brown <chbphone55@gmail.com>
2018-11-30 11:39:38 -0700

    yeet

--- src/components/movie/index.js
+++ src/components/movie/index.js
@@ -2,6 +2,8 @@
 import { h, Component } from 'preact';
 import style from './style.css';
 
+const isString = value => typeof value === 'string'
+
 export default class Movie extends Component {
   render() {
     return (
@@ -10,5 +12,23 @@ export default class Movie extends Component {
         <figcaption>{this.props.movieName}</figcaption>
       </figure>
     );
+  }
+
+  static propTypes = {
+    posterSrc: {
+      checker: isString,
+      message: 'posterSrc must be a string',
+      isRequired: true
+    },
+    posterAlt: {
+      checker: isString,
+      message: 'posterAlt must be a string',
+      isRequired: true
+    },
+    movieName: {
+      checker: isString,
+      message: 'movieName must be a string',
+      isRequired: true
+    }
   }
 }
--- src/routes/movies/index.js
+++ src/routes/movies/index.js
@@ -1,13 +1,12 @@
 import { h, Component } from 'preact';
-import Card from 'preact-material-components/Card';
-import 'preact-material-components/Card/style.css';
-import 'preact-material-components/Button/style.css';
-import style from './style';
+import Movie from '../../components/movie';
+import style from './style.css';
 
 export default class Movies extends Component {
   render() {
     return (
       <div class={`${style.movies} page`}>
+        <Movie posterSrc="https://picsum.photos/400/300" posterAlt="placeholder" movieName="Movie Title" />
       </div>
     );
   }
