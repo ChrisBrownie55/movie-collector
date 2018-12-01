033f3b6586b452f58fdf07a5b8ef75e80c8f8457
Christopher Brown <chbphone55@gmail.com>
2018-11-30 21:08:06 -0700

    movies and login routes converted

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -1,4 +1,5 @@
 import { h, Component } from 'preact';
+import { login } from '../../store';
 
 import Button from 'preact-material-components/Button';
 import 'preact-material-components/Button/style.css';
@@ -14,7 +15,7 @@ export default class Login extends Component {
         <div class={style.Login}>
           <Typography headline5>Welcome to Movie Collector</Typography>
           <Typography body1>If you haven't registered yet, don't worry, logging in with Google will create you an account.</Typography>
-          <Button class={style.googleSignIn} onClick={this.props.onLogin}>
+          <Button class={style.googleSignIn} onClick={login}>
             <img src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg" alt="Google logo" />
             <span>Sign in with Google</span>
           </Button>
--- src/routes/movies/index.js
+++ src/routes/movies/index.js
@@ -1,22 +1,27 @@
 import { h, Component } from 'preact';
-import { route } from 'preact-router';
+import { route } from '../../store';
+import { connect } from 'preact-redux';
 
 import Icon from 'preact-material-components/Icon';
 
 import style from './style.css';
 
-export default class Movies extends Component {
+class Movies extends Component {
   goToSearch = () => route('/search');
 
-  render() {
+  render({ user: { photoURL, displayName } }) {
     return (
       <div class={`${style.movies} page`}>
         <header class={style.header}>
-          <img class={style.avatar} src={this.props.user.photoURL} alt="avatar" />
+          <img class={style.avatar} src={photoURL} alt="avatar" />
           <Icon class={style.search} onClick={this.goToSearch}>search</Icon>
         </header>
-        <h1 class={style.displayName}>{this.props.user.displayName.split(' ')[0]}'s Movies</h1>
+        <h1 class={style.displayName}>{displayName.split(' ')[0]}'s Movies</h1>
       </div>
     );
   }
 }
+
+export default connect(
+  ({ user }) => ({ user })
+)(Movies);
