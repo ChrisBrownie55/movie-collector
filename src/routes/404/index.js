d3eafa5eb4ee14aaf09058da1674de96b09594a5
Christopher Brown <chbphone55@gmail.com>
2018-11-30 21:08:36 -0700

    remove unused imports

--- src/routes/404/index.js
+++ src/routes/404/index.js
@@ -1,6 +1,4 @@
 import { h, Component } from 'preact';
-import Card from 'preact-material-components/Card';
-import 'preact-material-components/Card/style.css';
 import style from './style';
 
 export default class NotFound extends Component {
