a3a5c9f116e8fad8145dbd6565519755f2fab557
Christopher Brown <chbphone55@gmail.com>
2018-11-30 17:25:37 -0700

    proptypes and basic layout

---
+++ src/components/moviesList/index.js
@@ -0,0 +1,36 @@
+import { h, Component } from 'preact';
+import Movie from '../movie';
+
+import style from './style.css';
+
+const isFunction = value => value instanceof Function;
+
+export default class MoviesList extends Component {
+  render() {
+    return (
+      <section class={style.moviesList}>
+        {
+
+        }
+      </section>
+    )
+  }
+
+  static propTypes = {
+    movies: {
+      checker: value => Array.isArray(value) && value.every(v => v instanceof Object),
+      message: 'movies must be an array of objects',
+      isRequired: true
+    },
+    onRemoveFromLibrary: {
+      checker: isFunction,
+      message: 'onRemoveFromLibrary must be a function',
+      isRequired: true
+    },
+    onAddToLibrary: {
+      checker: isFunction,
+      message: 'onAddToLibrary must be a function',
+      isRequired: true
+    }
+  }
+}
--- src/routes/movies/index.js
+++ src/routes/movies/index.js
@@ -3,7 +3,6 @@ import { route } from 'preact-router';
 
 import Icon from 'preact-material-components/Icon';
 
-import Movie from '../../components/movie';
 import style from './style.css';
 
 export default class Movies extends Component {
@@ -17,10 +16,6 @@ export default class Movies extends Component {
           <Icon class={style.search} onClick={this.goToSearch}>search</Icon>
         </header>
         <h1 class={style.displayName}>{this.props.user.displayName.split(' ')[0]}'s Movies</h1>
-        <section class={style.moviesList}>
-          <Movie posterSrc="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPcvWv3fwAHmQMx4nFrOwAAAABJRU5ErkJggg==" posterAlt="placeholder" movieName="Movie Title" />
-          <Movie posterSrc="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPcvWv3fwAHmQMx4nFrOwAAAABJRU5ErkJggg==" posterAlt="placeholder" movieName="Movie Title" />
-        </section>
       </div>
     );
   }
