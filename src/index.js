92fc7c009e6e26e0e131c6c0a128101fd27ac590
Christopher Brown <chbphone55@gmail.com>
2018-12-04 15:51:58 -0700

    fixes bug where authentication from PWA opens new browser and stays there

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+// fixes bug where authentication from PWA opens new browser and stays there.
+const ios = !!navigator.platform && /iPhone/.test(navigator.platform);
+if (ios) {
+  document.querySelector('link[rel="manifest"]').setAttribute('rel', 'no-on-ios');
+}
+
 import './style/index.css';
 import App from './components/app';
 
